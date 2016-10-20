00c8ef5cc64540392d29c5fe035a1245eaa85b57
Mirek Lechowski <mirek@platan>
2016-10-20 13:11:48 +0200

    4th release

--- db/vaisala-seneca.js
+++ db/vaisala-seneca.js
@@ -6,26 +6,21 @@ const seneca = require('seneca')();
 seneca.use(require('seneca-basic')).use(require('seneca-entity'));
 
 seneca.add('role:entities,cmd:save', (msg, respond) => {
-    let entity = seneca.make$('entity');
-    let dt = new Date();
-    entity.dt = dt.getUTCFullYear() + pad0(dt.getUTCMonth()) + pad0(dt.getUTCDate()) + 
-        pad0(dt.getUTCHours()) + pad0(dt.getUTCMinutes()) + pad0(dt.getUTCSeconds());  
-    entity.data = msg.data;
-    entity.save$((err, ent) => {
-        respond(err, { value: ent.id });
-    })
+    seneca
+        .make$('entity')
+        .data$({
+            dt: getFormattedUTCDateTime(),
+            data: msg.data
+        })
+        .save$(respond);
 });
 
 seneca.add('role:entities,cmd:load', (msg, respond) => {
-    seneca.make$('entity').load$(msg.id, (err, ent) => {
-        respond(err, { value: ent });
-    })    
+    seneca.make$('entity').load$(msg.id, respond);    
 });
 
 seneca.add('role:entities,cmd:list', (msg, respond) => {
-    seneca.make$('entity').list$({}, (err, list) => {
-        respond(err, { value: list });
-    })    
+    seneca.make$('entity').list$({}, respond);
 });
 
 seneca.add('role:entities,cmd:ready', (msg, respond) => {
@@ -48,20 +43,23 @@ let checkerId = setInterval(() => {
 
         seneca.act('role:entities,cmd:ready', (err, respond) => {
             if (err) console.log(err);
-            else console.log("Respond ready: " + respond.value);
+            else console.log("Respond ready: ", respond.value);
         });                
 
         for (let i = 0; i < 10; i++) {
             seneca.act('role:entities,cmd:save', (err, respond) => {
                 if (err) console.log(err);
-                else { 
-                    console.log("Respond save: " + respond.value);
+                else {
+                    console.log("Respond save: " + respond);
                     if (i == 9) {
                         seneca.act('role:entities,cmd:list', (err, respond) => {
                             if (err) console.log(err);
                             else {
-                                let list = respond.value;
-                                list.forEach( (ent) => { console.log(ent.data$(false)); } );
+                                respond.forEach( (ent) => {
+                                    seneca.act('role:entities,cmd:load', { id: ent.id }, (err, respond) => {
+                                        console.log("Respond load: ", respond.data$(false));
+                                    });            
+                                });
                             }
                         });
                     }                                   
@@ -74,6 +72,11 @@ let checkerId = setInterval(() => {
 const pad0 = (value) => {
     if (value < 10) return '0' + value;
     return value;
+}
+
+const getFormattedUTCDateTime = () => {
+    let dt = new Date();
+    return dt.getUTCFullYear() + pad0(dt.getUTCMonth()) + pad0(dt.getUTCDate()) + pad0(dt.getUTCHours()) + pad0(dt.getUTCMinutes()) + pad0(dt.getUTCSeconds());  
 }
 
 
